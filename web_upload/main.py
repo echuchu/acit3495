77125b46bd24440dfbb46cccfc8a914e76f6ad46
zafyyre <zaaf17@hotmail.com>
2023-10-03 18:46:18 -0700

    Added files for web_streaming and made changes in web_upload

--- web_upload/main.py
+++ web_upload/main.py
@@ -1,30 +1,18 @@
-from distutils.log import debug
-from fileinput import filename
-import mysql.connector
-from flask import *
+from flask import *  
+import os
 
-app = Flask(__name__)
-
-# credentials = {
-#         'user': 'video_user',
-#         'password': 'video_password',
-#         'host': 'mysql',
-#         'port': '3306',
-#         'database': 'video_database'
-#     }
-
-# connection = mysql.connector.connect(**credentials)
-
-@app.route('/')
-def main():
-    return render_template("./templates/index.html")
-
-@app.route('/success', methods = ['POST'])
-def success():
-    if request.method == 'POST':
+app = Flask(__name__)  
+  
+@app.route('/')  
+def main():  
+    return render_template("index.html")  
+  
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
         f = request.files['file']
-        f.save(f.filename)
-        return render_template("./templates/Acknowledgement.html", name = f.filename)  
+        f.save(os.path.join('videos', f.filename))
+        return render_template("Acknowledgement.html", name = f.filename)  
   
 if __name__ == '__main__':  
     app.run(debug=True)
