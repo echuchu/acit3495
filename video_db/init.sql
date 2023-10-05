0310e1835388667fa637435170934e57f5e895d0
Eva Chu <echu58@my.bcit.ca>
2023-10-04 20:05:46 -0700

    initial filesys

--- video_db/init.sql
+++ video_db/init.sql
@@ -2,13 +2,9 @@ CREATE DATABASE video;
 
 USE video;
 
-CREATE USER 'video_read_write'@'%' IDENTIFIED BY 'password';
+CREATE USER 'video_user'@'%' IDENTIFIED BY 'password';
 
-GRANT SELECT, INSERT, UPDATE ON video.* TO video_read_write;
-
-CREATE USER 'video_read_only'@'%' IDENTIFIED BY 'password';
-
-GRANT SELECT ON video.* TO video_read_only;
+GRANT SELECT, INSERT, UPDATE ON video.* TO video_user;
 
 CREATE TABLE video_files (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
